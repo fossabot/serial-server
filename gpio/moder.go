538497e7c6fdb100baf7a47155afdc54d4162f7b
Ivan Kozelskikh <johndemigod3318@gmail.com>
2020-07-07 22:37:54 +0500

    pin range check

--- gpio/moder.go
+++ gpio/moder.go
@@ -2,8 +2,12 @@ package gpio
 
 type Moder uint32
 
-func (r *Moder) Set(pinOffset uint8, state moderState) {
+func (r *Moder) Set(pinOffset uint8, state moderState) error {
+	if pinOffset > 31 {
+		return ErrPinNotExists
+	}
 	*r |= Moder(state << (pinOffset * MODE_PIN_SIZE))
+	return nil
 }
 
 func (r Moder) Test(pinOffset uint8, state moderState) bool {
