1923f0eefc005199b9738bd486925e454fe643cf
Ivan Kozelskikh <johndemigod3318@gmail.com>
2020-07-07 23:03:18 +0500

    otype register

---
+++ gpio/otyper.go
@@ -0,0 +1,24 @@
+package gpio
+
+type otyperState uint8
+
+const (
+	OTYPE_PUSHPULL  otyperState = 0b00 // Output push-pull (reset state)
+	OTYPE_OPENDRAIN otyperState = 0b01 // Output open-drain
+)
+
+const OTYPE_RESET uint32 = 0
+
+type Otyper uint32
+
+func (r *Otyper) Set(pinOffset uint8, state otyperState) error {
+	if pinOffset > 15 {
+		return ErrPinNotExists
+	}
+	*r |= Otyper(state << pinOffset)
+	return nil
+}
+
+func (r Otyper) Test(pinOffset uint8, state otyperState) bool {
+	return (uint32(r)>>pinOffset)&uint32(state) == uint32(state)
+}
