2fd42003918ab648330fa48021df8dfd380a927a
Ivan Kozelskikh <johndemigod3318@gmail.com>
2020-07-08 20:53:45 +0500

    public port fields, factory port a

--- gpio/port.go
+++ gpio/port.go
@@ -2,18 +2,34 @@ package gpio
 
 type port struct {
 	baseAddress uint32
-	moder       Moder
-	otyper      Otyper
-	ospeedr     Ospeedr
-	pupdr       Pupdr
-	idr         Idr
-	odr         Odr
-	bssr        Bsrr
-	lckr        Lckr
-	afrl        Afrl
-	afrh        Afrh
+	Moder
+	Otyper
+	Ospeedr
+	Pupdr
+	Idr
+	Odr
+	Bsrr
+	Lckr
+	Afrl
+	Afrh
 }
 
 const (
 	BASE_ADDRESS_PORT_A uint32 = 0x4002_0000
 )
+
+func NewPortA() *port {
+	return &port{
+		baseAddress: BASE_ADDRESS_PORT_A,
+		Moder:       Moder(MODER_RESET_A),
+		Otyper:      Otyper(OTYPER_RESET),
+		Ospeedr:     Ospeedr(OSPEEDR_RESET_A),
+		Pupdr:       Pupdr(PUPDR_RESET_A),
+		Idr:         Idr(IDR_RESET),
+		Odr:         Odr(ODR_RESET),
+		Bsrr:        Bsrr(BSRR_RESET),
+		Lckr:        Lckr(LCKR_RESET),
+		Afrl:        Afrl(AFRL_RESET),
+		Afrh:        Afrh(AFRH_RESET),
+	}
+}
