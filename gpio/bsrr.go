0a5737d2415047867cb296356f391453932c635d
Ivan Kozelskikh <johndemigod3318@gmail.com>
2020-07-08 20:52:43 +0500

    refactoring setters, rename constants

--- gpio/bsrr.go
+++ gpio/bsrr.go
@@ -3,8 +3,8 @@ package gpio
 type bsrrState uint8
 
 const (
-	BSRR_NOACTION  bsrrState = 0b00 // No action on the corresponding ODRx bit
-	BSRR_SET_RESET bsrrState = 0b01 // Set/Reset the corresponding ODRx bit
+	BSR_NOACTION bsrrState = 0b00 // No action on the corresponding ODRx bit
+	BSR_ACTION   bsrrState = 0b01 // Set/Reset the corresponding ODRx bit
 )
 
 const BSRR_RESET uint32 = 0
@@ -20,17 +20,13 @@ func (r *Bsrr) SetBs(pinOffset uint8, state bsrrState) error {
 }
 
 func (r *Bsrr) SetBr(pinOffset uint8, state bsrrState) error {
-	if pinOffset > 15 {
-		return ErrPinNotExists
-	}
-	*r |= Bsrr(state << (pinOffset + 15))
-	return nil
+	return r.SetBs(pinOffset+15, state)
 }
 
 func (r Bsrr) TestBs(pinOffset uint8, state bsrrState) bool {
-	return (uint32(r)>>(pinOffset))&uint32(state) == uint32(state)
+	return (uint32(r)>>pinOffset)&uint32(state) == uint32(state)
 }
 
 func (r Bsrr) TestBr(pinOffset uint8, state bsrrState) bool {
-	return (uint32(r)>>(pinOffset+15))&uint32(state) == uint32(state)
+	return r.TestBs(pinOffset+15, state)
 }
